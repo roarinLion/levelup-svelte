f3c9c35825e3576a55a82dee157f7dc4b5a26ff8
Roarin Lion <roarinlion@pm.me>
2023-08-19 12:50:32 -0700

    Add Modal component with fade transition
    This commit adds a new file `Modal.svelte` which contains a modal component with fade transition. The modal is displayed as a fixed position element in the center of the screen and has a dark overlay background. The modal itself has a dark background, padding, border-radius, and box-shadow to give it a card-like appearance. The fade transition is applied when the modal enters or exits the DOM, with a duration of 250 milliseconds.

---
+++ src/Modal.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { fade } from 'svelte/transition'
+</script>
+
+<div
+  out:fade={{ duration: 250 }}
+  in:fade={{ duration: 250 }}
+  class="wrapper"
+>
+  <div class="modal">
+    <!-- <slot>optional fallback</slot> -->
+
+    <slot />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(8, 8, 8, 0.6);
+  }
+  .modal {
+    position: fixed;
+    background: rgb(31, 31, 31);
+    padding: 20px;
+    border-radius: 15px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  }
+</style>
